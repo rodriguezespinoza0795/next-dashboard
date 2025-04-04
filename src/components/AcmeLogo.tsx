e94680908e7f02121516d999642a361b5a4fc0c7
davidre0795 <davidre0795@gmail.com>
2025-04-03 21:00:14 -0600

    section 2 completed

---
+++ src/components/AcmeLogo.tsx
@@ -0,0 +1,13 @@
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "@/config/fonts";
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+    </div>
+  );
+}
