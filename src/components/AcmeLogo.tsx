c1cd9fc6d3f1264f8725856cff46e4561d45d312
davidre0795 <davidre0795@gmail.com>
2025-04-03 21:38:58 -0600

    section 3 completed

--- src/components/AcmeLogo.tsx
+++ src/components/AcmeLogo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+      <p className="text-[44px] ">Acme</p>
     </div>
   );
 }
